93e3591269aa18ba3ddf44cf7a6a1ed8a46cfb46
ahotorino <edoardo.fantolino@gmail.com>
2026-06-06 09:44:03 +0200

    Logger in console and in file

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,24 @@
 import logging
 
 def get_logger():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-    return logging.getLogger("etl")
+    logger = logging.getLogger("etl")
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
+
+        # Console
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+
+        # File
+        file_handler = logging.FileHandler("../logs/etl.log")
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+
+    return logger
