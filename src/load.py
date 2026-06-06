c17d943fa32da8c0dacb2c94d77d71235524dce7
ahotorino <edoardo.fantolino@gmail.com>
2026-06-06 08:50:05 +0200

    Add basic load function

--- src/load.py
+++ src/load.py
@@ -1,6 +1,26 @@
-import pandas as pd
+from sqlalchemy import create_engine
 
-print("We can do lots of stuff with pandas")
-df = pd.read_csv("../data/raw/transactions.csv")
+def load(df_valid, df_rejected, df_run, db_path):
 
-print(df.head())
+    engine = create_engine(f"sqlite:///{db_path}")
+
+    df_valid.to_sql(
+        "transactions", 
+        engine, 
+        if_exists="replace", 
+        index=False)
+    
+    # INVALID data
+    df_rejected.to_sql(
+        "rejected_transactions",
+        engine,
+        if_exists="replace",
+        index=False
+    )
+
+    df_run.to_sql(
+        "etl_runs",
+        engine,
+        if_exists="append",
+        index=False
+    )
